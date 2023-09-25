7ac2a0cbd815bff42674fb9e1f06fd659a102b59
WenbooSu <105067670+WenbooSu@users.noreply.github.com>
2023-09-25 00:05:21 -0400

    Update game_engine.py
    Update

--- game_engine.py
+++ game_engine.py
@@ -1,3 +1,6 @@
+import keyboard
+
+
 class Units:
 
     def __init__(self, faction, unit_name):
@@ -66,6 +69,9 @@ class CheckerBoard:
                     print(' * ', end=' ')
             print()'''
 
+    def get_board(self):
+        return self.board
+
     def show_board(self):
         row_labels = "ABCDE"
         print("    0   1   2   3   4")
@@ -86,43 +92,263 @@ class CheckerBoard:
                         return i, j
         return None
 
-    # print((obj1.get_position('aP9')))
+    # switch_position only switches a Unit node and a None node
+    def switch_position(self, current_position, to_destination):
+        self.board[current_position[0]][current_position[1]], self.board[to_destination[0]][to_destination[1]] = \
+            self.board[to_destination[0]][to_destination[1]], self.board[current_position[0]][current_position[1]]
 
     # this function can only be used in move()
-    def position_occupied(self, row, col):
-        if self.board[row][col] is not None:
+    def position_occupied(self, tup):
+        if self.board[tup[0]][tup[1]] is not None:
             return True
         else:
             return False
 
     # print(obj1.position_occupied(2, 4))
-
+    # if engaged -> AI, Firewall, Program cannot move
     def engaged(self, tup):
         directions = [(tup[0], tup[1] + 1), (tup[0], tup[1] - 1), (tup[0] + 1, tup[1]), (tup[0] - 1, tup[1])]
         for x, y in directions:
-            if x < 5 and y < 5:
+            if 0 <= x < 5 and 0 <= y < 5:
                 if isinstance(self.board[x][y], Units):
                     if self.board[x][y].faction != self.board[tup[0]][tup[1]].faction:
                         return True
         return False
 
+    # Assume to enter [A] [1] as the selection of unit
+    # def move_to(self, position):
+
+
+class Engine:
+
+    def __init__(self):
+        self.game_map = CheckerBoard()
+
+    def get_game_map(self):
+        return self.game_map
+
+    # Return the Index of a Unit on Game Board
+    @staticmethod
+    def select_unit():
+        user_input = input("Choose Your Unit: ")
+        while len(user_input) != 2 or user_input[0].upper() not in ['A', 'B', 'C', 'D', 'E'] \
+                or not user_input[1].isnumeric() or int(user_input[1]) < 1 or int(user_input[1]) > 5:
+            print("Invalid Input. Please Enter a Proper Position: ")
+            user_input = input("Choose Your Unit: ")
+        return convert2board_index(user_input[0].upper(), int(user_input[1]))
+
+    def get_unit_node(self, pos_tup):
+        return self.get_game_map().get_board()[pos_tup[0]][pos_tup[1]]
+
+    # Parameters are tuple only
+    def move(self, my_position):
+        command = ''
+        while True:
+            print("Your Command: ")
+            if self.get_unit_node(my_position).get_unit_name() in ['Techs', 'Viruses']:
+                command = keyboard_command()
+                if command == 'up':  # [x][y] -> [x-1][y]
+                    new_position = (my_position[0] - 1, my_position[1])
+                    if self.get_game_map().position_occupied(new_position):
+                        print("Invalid Move: a Unit on this Position")
+                        continue
+                    else:
+                        if within_range(new_position):
+                            self.get_game_map().switch_position(my_position, new_position)
+                            self.get_game_map().show_board()
+                            break
+                        else:
+                            print('Invalid Move: Out of Range')
+                if command == 'down':  # [x][y] -> [x+1][y]
+                    new_position = (my_position[0] + 1, my_position[1])
+                    if self.get_game_map().position_occupied(new_position):
+                        print("Invalid Move: a Unit on this Position")
+                        continue
+                    else:
+                        if within_range(new_position):
+                            self.get_game_map().switch_position(my_position, new_position)
+                            self.get_game_map().show_board()
+                            break
+                        else:
+                            print('Invalid Move: Out of Range')
+                if command == 'left':  # [x][y] -> [x][y-1]
+                    new_position = (my_position[0], my_position[1] - 1)
+                    if self.get_game_map().position_occupied(new_position):
+                        print("Invalid Move: a Unit on this Position")
+                        continue
+                    else:
+                        if within_range(new_position):
+                            self.get_game_map().switch_position(my_position, new_position)
+                            self.get_game_map().show_board()
+                            break
+                        else:
+                            print('Invalid Move: Out of Range')
+                if command == 'right':  # [x][y] -> [x][y+1]
+                    new_position = (my_position[0], my_position[1] + 1)
+                    if self.get_game_map().position_occupied(new_position):
+                        print("Invalid Move: a Unit on this Position")
+                        continue
+                    else:
+                        if within_range(new_position):
+                            self.get_game_map().switch_position(my_position, new_position)
+                            self.get_game_map().show_board()
+                            break
+                        else:
+                            print('Invalid Move: Out of Range')
+            if self.get_unit_node(my_position).get_unit_name() in ['AI', 'Firewalls', 'Programs']:
+                command = keyboard_command()
+                if self.get_game_map().engaged(my_position):
+                    print("This Unit is Engaged in Combat")
+                    break
+                else:
+                    if self.get_unit_node(my_position).get_faction() == 'defender':
+                        if command not in ['down', 'right']:
+                            print('Illegal Move for Defender')
+                            continue
+                        else:
+                            if command == 'down':
+                                new_position = (my_position[0] + 1, my_position[1])
+                                if self.get_game_map().position_occupied(new_position):
+                                    print("Invalid Move: a Unit on this Position")
+                                    continue
+                                else:
+                                    if within_range(new_position):
+                                        self.get_game_map().switch_position(my_position, new_position)
+                                        self.get_game_map().show_board()
+                                        break
+                                    else:
+                                        print('Invalid Move: Out of Range')
+                            if command == 'right':
+                                new_position = (my_position[0], my_position[1] + 1)
+                                if self.get_game_map().position_occupied(new_position):
+                                    print("Invalid Move: a Unit on this Position")
+                                    continue
+                                else:
+                                    if within_range(new_position):
+                                        self.get_game_map().switch_position(my_position, new_position)
+                                        self.get_game_map().show_board()
+                                        break
+                                    else:
+                                        print('Invalid Move: Out of Range')
+                    if self.get_unit_node(my_position).get_faction() == 'attacker':
+                        if command not in ['up', 'left']:
+                            print('Illegal Move for Attacker')
+                            continue
+                        else:
+                            if command == 'up':
+                                new_position = (my_position[0] - 1, my_position[1])
+                                if self.get_game_map().position_occupied(new_position):
+                                    print("Invalid Move: a Unit on this Position")
+                                    continue
+                                else:
+                                    if within_range(new_position):
+                                        self.get_game_map().switch_position(my_position, new_position)
+                                        self.get_game_map().show_board()
+                                        break
+                                    else:
+                                        print('Invalid Move: Out of Range')
+                            if command == 'left':
+                                new_position = (my_position[0], my_position[1] - 1)
+                                if self.get_game_map().position_occupied(new_position):
+                                    print("Invalid Move: a Unit on this Position")
+                                    continue
+                                else:
+                                    if within_range(new_position):
+                                        self.get_game_map().switch_position(my_position, new_position)
+                                        self.get_game_map().show_board()
+                                        break
+                                    else:
+                                        print('Invalid Move: Out of Range')
+
+    # Better Structured move() -> to be decent a guy
+    """def move(self, my_position):
+        def is_valid_move(new_position):
+            if not self.get_game_map().position_occupied(new_position):
+                if within_range(new_position):
+                    self.get_game_map().switch_position(my_position, new_position)
+                    return True
+                else:
+                    print('Invalid Move: Out of Range')
+            else:
+                print("Invalid Move: a Unit on this Position")
+            return False
+    
+        command = ''
+        unit_name = self.get_unit_node(my_position).get_unit_name()
+        faction = self.get_unit_node(my_position).get_faction()
+    
+        while True:
+            print("Your Command: ")
+            command = keyboard_command()
+    
+            if unit_name in ['Techs', 'Viruses']:
+                if command == 'up':  # [x][y] -> [x-1][y]
+                    new_position = (my_position[0] - 1, my_position[1])
+                elif command == 'down':  # [x][y] -> [x+1][y]
+                    new_position = (my_position[0] + 1, my_position[1])
+                elif command == 'left':  # [x][y] -> [x][y-1]
+                    new_position = (my_position[0], my_position[1] - 1)
+                elif command == 'right':  # [x][y] -> [x][y+1]
+                    new_position = (my_position[0], my_position[1] + 1)
+                else:
+                    print("Invalid Move: Unsupported command for this unit type")
+                    continue
+    
+                if is_valid_move(new_position):
+                    break
+    
+            elif unit_name in ['AI', 'Firewalls', 'Programs']:
+                if faction == 'defender':
+                    valid_commands = ['down', 'right']
+                else:
+                    valid_commands = ['up', 'left']
+    
+                if command not in valid_commands:
+                    print(f'Illegal Move for {faction.capitalize()}')
+                    continue
+    
+                if command == 'up':
+                    new_position = (my_position[0] - 1, my_position[1])
+                elif command == 'down':
+                    new_position = (my_position[0] + 1, my_position[1])
+                elif command == 'left':
+                    new_position = (my_position[0], my_position[1] - 1)
+                elif command == 'right':
+                    new_position = (my_position[0], my_position[1] + 1)
+    
+                if is_valid_move(new_position):
+                    break"""
+    
 
 # static functions
-def convert2tuple(char, col):
-    var = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+def convert2board_index(char, col):
+    tup_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
     # if not in var -> throw exception
-    row = var[char]
+    row = tup_dict[char]
     return row, col
 
 
-obj1 = CheckerBoard()
+def keyboard_command():
+    while True:
+        keyboard_event = keyboard.read_event(suppress=True)
+        if keyboard_event.event_type == keyboard.KEY_DOWN:
+            if keyboard_event.name in ['left', 'right', 'up', 'down']:
+                return keyboard_event.name
+            else:
+                print("Invalid Keyboard Input")
 
-obj1.show_board()
 
-print((obj1.get_position('aP9')))
+def within_range(tup):
+    if 0 <= tup[0] < 5 and 0 <= tup[1] < 5:
+        return True
+    else:
+        return False
 
-print(convert2tuple('C', 4))
 
-print(obj1.position_occupied(2, 4))
-
-print(obj1.engaged((2, 4)))
+var = Engine()
+var.get_game_map().show_board()
+print()
+position = var.select_unit()
+var.move(position)
+print()
+var.get_game_map().show_board()
