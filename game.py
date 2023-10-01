999633772bb327292bca641a9af9f5c0665cadf7
WenbooSu <105067670+WenbooSu@users.noreply.github.com>
2023-09-30 23:33:54 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ import game_engine as ge
 class MainGameLoop:
     def __init__(self):
         self.engine = ge.Engine()
-        self.max_moves = 100
-        self.current_move = 0
+        self.max_round = 100
+        self.current_round = 1
         self.player1 = 'attacker'
         self.player2 = 'defender'
 
@@ -20,7 +20,7 @@ class MainGameLoop:
         print('                                                         credit to: CloseAI Ultd\n')
         mode = input('Enter The Mode Number: ')
         if mode not in [1, 2, 3]:
-            sys.exit()
+            return 1
         else:
             return mode
 
@@ -31,44 +31,56 @@ class MainGameLoop:
             self.human_ai()
 
     def play_game_human_human(self):
-        while self.current_move <= 100:
+        while self.current_round <= 100 or self.engine.get_ai_dead():
             # Check game-ending conditions: AI dead or max moves reached
             if self.engine.get_ai_dead():
-                print("Attacker wins!")
-                break
-            if self.current_move >= self.max_moves:
+                if self.current_round % 2 == 0:
+                    print(f'Defender Wins in {self.current_round} Moves')
+                    break
+                else:
+                    print(f'Attacker Wins in {self.current_round} Moves')
+                    break
+            if self.current_round >= self.max_round:
                 print("Maximum moves reached. A draw game.")
                 break
 
             # Display game board
-            print("Current Game Board:")
+            print("\nCurrent Game Board:")
             self.engine.get_game_map().show_board()
-            print(f"Move {self.current_move + 1}")  # Count the move start from 1
-
+            # print(f"Move {self.current_move + 1}")  # Count the move start from 1
+            # 0 1 2 3 4 5 6
             # Determine current player
-            current_player = 'defender' if self.current_move % 2 == 0 else 'attacker'  # Attacker goes first
-            print(f"Current Player: {current_player}")
+            current_player = 'defender' if self.current_round % 2 == 0 else 'attacker'  # Attacker goes first
+            print(f"\nCurrent Player: {current_player}")
 
             # Choose player action
             while True:
+                print(f'ROUND: {self.current_round}')
 
-                s_position = self.engine.select_unit()
-
-                if self.engine.get_unit_node((s_position[0], s_position[1])).get_faction() != current_player:
-                    print('Cannot Select Enemy''s Unit')
-                    continue
-
-                print("\nPlease choose an action:")
-                print("1. Move")
-                print("2. Attack")
-                print("3. Repair")
-                print("4. Self-Destruct")
-                print("5. Select Another Unit\n")
-                action_choice = input("Enter the number of your choice from integer 1-4: ")
-                if action_choice in ['1', '2', '3', '4', '5']:
+                while True:
+                    s_position = self.engine.select_unit()
+                    if self.engine.get_unit_node((s_position[0], s_position[1])) is None:
+                        print('Cannot Select None Unit')
+                        continue
+                    if self.engine.get_unit_node((s_position[0], s_position[1])).get_faction() != current_player:
+                        print('Cannot Select Enemy''s Unit')
+                        continue
                     break
-                else:
-                    print("Invalid choice. Please select a valid action from integer 1-5.")
+
+                action_choice = 0
+                while True:
+                    print("\nPlease choose an action:")
+                    print("1. Move")
+                    print("2. Attack")
+                    print("3. Repair")
+                    print("4. Self-Destruct")
+                    print("5. Select Another Unit\n")
+                    action_choice = input("Enter the number of your choice from integer 1-4: ")
+                    if action_choice in ['1', '2', '3', '4', '5']:
+                        break
+                    else:
+                        print("Invalid choice. Please select a valid action from integer 1-5.")
+                        continue
 
                 if action_choice == '1':  # Move
                     if self.engine.movable(s_position):
@@ -98,17 +110,13 @@ class MainGameLoop:
                     break
 
             # Increment move counter
-            self.current_move += 1
+            self.current_round += 1
 
     def human_ai(self):
-        print(self.engine.get_game_map()) # To Do
+        print(self.engine.get_game_map())  # To Do
 
-    def main_game_loop(self):
-        attacker_pos = None
-        defender_pos = None
-        turn = 0
-        print('Attacker Starts First')
-        while not self.engine.get_ai_dead():
-            attacker_pos = self.engine.select_unit()
-            while self.engine.get_unit_node(attacker_pos).get_faction() != 'attacker':
-                continue
+
+if __name__ == "__main__":
+    game = MainGameLoop()
+    game_mode = game.main_menu_page()
+    game.start_game(game_mode)
